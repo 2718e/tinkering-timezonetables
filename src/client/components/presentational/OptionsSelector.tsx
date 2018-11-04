86b8b6ad439569edde8f350e9901b8a38e74ea77
Dan <livelyandexciting@gmail.com>
2018-11-04 13:17:32 +1300

    switch datepicker to react-dates

--- src/client/components/presentational/OptionsSelector.tsx
+++ src/client/components/presentational/OptionsSelector.tsx
@@ -2,11 +2,11 @@ import { ZoneDisplayConfig } from '../../datatypes';
 import * as React from 'react'
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import DatePicker from 'react-datepicker';
 import * as moment from 'moment-timezone'
-import {DATE_FORMAT} from '../../helpers'
-
-import 'react-datepicker/dist/react-datepicker.css';
+import { DATE_FORMAT } from '../../helpers'
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/initialize'
+import 'react-dates/lib/css/_datepicker.css'
 
 export type OptionsSelectorProps = {
     currentConfig: ZoneDisplayConfig
@@ -14,32 +14,41 @@ export type OptionsSelectorProps = {
     onChangeDate: (nextValue: boolean) => void
 }
 
-// from https://stackoverflow.com/a/50392605 - trying to prevent datepicker opening soft keyboard on mobile by setting readonly
-const CustomDatePickerInput = ({onChange, placeholder, value, isSecure, id, onClick}: any) => (
-    <input type='text' readOnly={true}
-        onChange={onChange}
-        placeholder={placeholder}
-        value={value}
-        id={id}
-        onClick={onClick}
-    />
-);
+type OptionsSelectorState = {
+    datePickerFocused: boolean
+}
 
-export const OptionsSelector = (props: OptionsSelectorProps) =>
-    <div >
-        <div key="24hr">
-        <FormControlLabel labelPlacement='start' control={
-            <Switch color="primary" checked={props.currentConfig.use24hour}
-                onChange={e => props.onToggle24Hour(e.target.checked)} />}
-            label="24 hour time format" />
+export class OptionsSelector extends React.Component<OptionsSelectorProps, OptionsSelectorState>{
+
+    constructor(props: OptionsSelectorProps){
+        super(props)
+        this.state = {datePickerFocused: false}
+    }
+
+    render() {
+        const { onChangeDate, currentConfig, onToggle24Hour } = this.props
+        const { use24hour, dateInBaseZone } = currentConfig
+        return <div >
+            <div key="24hr">
+                <FormControlLabel labelPlacement='start' control={
+                    <Switch color="primary" checked={use24hour}
+                        onChange={e => onToggle24Hour(e.target.checked)} />}
+                    label="24 hour time format" />
+            </div>
+            <div key="date">
+                <FormControlLabel labelPlacement='start' control={
+                    <SingleDatePicker
+                        id="convertAtDateSelector"
+                        numberOfMonths={1}
+                        readOnly={true} small={true}
+                        isOutsideRange={()=>false}
+                        date={moment(dateInBaseZone, DATE_FORMAT)}
+                        focused={this.state.datePickerFocused} // PropTypes.bool
+                        onFocusChange={({ focused }) => this.setState({ datePickerFocused: focused })} // PropTypes.func.isRequired
+                        displayFormat={DATE_FORMAT}
+                        onDateChange={onChangeDate} />
+                } label="Convert at date?" />
+            </div>
         </div>
-        <div key="date">
-        <FormControlLabel labelPlacement='start' control={
-            <DatePicker
-                customInput={<CustomDatePickerInput />} 
-                selected={moment(props.currentConfig.dateInBaseZone, DATE_FORMAT)}
-                dateFormat={DATE_FORMAT}
-                onChange={props.onChangeDate} />
-        } label="Convert at date?" />
-        </div>
-    </div>
+    }
+}
