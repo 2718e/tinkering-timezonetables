2c8c9fdc40562c081f4d7b7c4eaa227269f3f615
Dan <livelyandexciting@gmail.com>
2018-11-04 13:31:46 +1300

    in dev allow browse from mobile device on same local network

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,13 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const ip = require('ip')
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist', port: 3185
+    contentBase: './dist',
+    port: 3185,
+    host: ip.address() // set webpack dev server host to the local machine ip address so can browse to it from mobile device on same lan
   }
 })
